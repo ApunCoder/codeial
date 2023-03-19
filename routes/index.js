ba66584b8d09e4a577b204bebae326b11c65852f
ApunCoder <nikhilkumar570@yahoo.com>
2023-03-19 16:35:35 +0530

    Adding some more routes and from now we set ejs

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,7 @@ console.log('router loaded');
 const homeController = require('../controllers/home_controller');
 
 router.get('/', homeController.home);
+router.use('/user',require('./user'));
+router.use('/user',require('./post'))
 
 module.exports = router;
